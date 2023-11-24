2b338d46f1d57c73fa72781783f3167d5cc28f7d
Rahul <rahul02dahiya@gmail.com>
2023-11-25 00:02:20 +0530

    learn context api forgot to add

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,29 @@
+import React from "react";
+import NoteContext from "./noteContext";
+import { useState } from 'react'
+
+
+const NoteState = (props)=>{
+
+   const s1={
+        "name" : "rahul",
+        "class": "CSb"
+    }
+
+    const [state,setState] = useState(s1)
+    const update = ()=>{
+        setTimeout(()=>{
+            setState({
+                "name":"Bits",
+                "class":"CSa"
+            })
+        },1000)
+    }
+    return(
+        <NoteContext.Provider value={{state,update}}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+}
+
+export default NoteState;
